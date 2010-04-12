849eb8126ca75bcfb5ce0d1fd4b8759236f5d023
homoludens <marko.homoludens@gmail.com>
2010-04-12 18:36:51 +0200

    removing unneeded code

--- feeds/views.py
+++ feeds/views.py
@@ -22,7 +22,12 @@ from django.contrib.auth.models import User
 
 from django.forms import ModelForm
 
+from django.http import HttpResponse
 
+from django.views.generic.create_update import create_object
+from django.views.generic.create_update import delete_object
+
+from django.core.urlresolvers import reverse
 
 from django.db.models.signals import post_save, pre_save
 import feedparser
@@ -35,60 +40,8 @@ def index(request):
 
 def stories(request, feed_id):
     p = get_object_or_404(Feed, pk=feed_id)
-    #all_stories = Story.objects.get(feed=feed_id)
     all_stories = Story.objects.filter(feed=feed_id)
     return render_to_response('feeds/stories.html', {'feed': all_stories, 'title': p})
-
-
-from django.http import HttpResponse
-
-def hello(request):
-    #return HttpResponse("Hello world")
-    #return HttpResponse("Welcome to the page at %s" % request.path)
-    #ua = request.META.get('HTTP_USER_AGENT', 'unknown')
-    #return HttpResponse("Your browser is %s" % ua)
-    values = request.META.items()
-    values.sort()
-    html = []
-    for k, v in values:
-        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
-    return HttpResponse('<table>%s</table>' % '\n'.join(html))
-
-
-from django.core.mail import send_mail
-#from django.http import HttpResponseRedirect
-#from django.shortcuts import render_to_response
-
-def contact(request):
-    errors = []
-    if request.method == 'POST':
-        if not request.POST.get('subject', ''):
-            errors.append('Enter a subject.')
-        if not request.POST.get('message', ''):
-            errors.append('Enter a message.')
-        if request.POST.get('email') and '@' not in request.POST['email']:
-            errors.append('Enter a valid e-mail address.')
-        if not errors:
-            send_mail(
-                request.POST['subject'],
-                request.POST['message'],
-                request.POST.get('email', 'noreply@example.com'),
-                ['siteowner@example.com'],
-            )
-            return HttpResponseRedirect('/contact/thanks/')
-    return render_to_response('contact_form.html', {
-        'errors': errors,
-        'subject': request.POST.get('subject', ''),
-        'message': request.POST.get('message', ''),
-        'email': request.POST.get('email', ''),
-    })
-
-
-
-from django.views.generic.create_update import create_object
-from django.views.generic.create_update import delete_object
-
-from django.core.urlresolvers import reverse
 
 
 #closure: http://en.wikipedia.org/wiki/Closure_%28computer_science%29
@@ -109,14 +62,6 @@ def make_feed_form(request):
 
     return FeedForm2
 
-
-
-class FeedForm(ModelForm):
-    class Meta:
-	 model=Feed
-	 #fields = ['url','title']
-	 #exclude=['title']
-	 exclude=['title', 'created_by']
 	 
 def feed_create(request):
   
